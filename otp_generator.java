4204984fd4a92b6a63ff80dfe2143b5f80134f9e
Misbah Anwar <127953074+misbah-anwar@users.noreply.github.com>
2023-10-04 08:24:10 +0530

    Update otp_generator.java

--- otp_generator.java
+++ otp_generator.java
@@ -1,33 +1,20 @@
 //author: Misbah
-//this code does not generate a fixed quantity of random numbers because some numbers are less than 8 digits.This needs to be improved
-import java.util.ArrayList;
+//improvised code
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Random;
 public class otp_generator {
-    public static void main(String[]args){
-        List<Integer>list1=new ArrayList<>();
-        List<Integer>list2=new ArrayList<>();
-        int otpsize=8;
-        int otpqty=10;
-        double rand1=0.0;
-        double rand2=0.0;
-        int rand3=0;
-        for(int i=0;i<otpqty*10;i++){
-            rand1=Math.random();
-            rand2=rand1*Math.pow(10,otpsize);
-            rand3=(int)rand2;
-            list1.add(rand3);
-        }
-        for(int i=0;i<otpqty;i++){
-            if(list1.get(i)>=Math.pow(10,otpsize-1)){
-                list2.add(list1.get(i));
+    public static void main(String[] args) {
+        List<String> list1 = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            String otp="";
+            for (int j = 0; j < 8 ; j++) {
+                otp += random.nextInt(10);
             }
+            list1.add(otp);
         }
-        int len2=list2.size();
-        System.out.println(len2);
-        for(int i=0;i<len2;i++){
-            System.out.println(list2.get(i));
-        }
+        System.out.println(list1);
     }
 }
+
