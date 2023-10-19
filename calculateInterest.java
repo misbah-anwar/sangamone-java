206bae963a40df8b7dbdfd07600284f185b319cd
Misbah Anwar <127953074+misbah-anwar@users.noreply.github.com>
2023-10-19 07:42:42 +0530

    Update calculateInterest.java

--- calculateInterest.java
+++ calculateInterest.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,59 +9,75 @@ import java.util.Scanner;
     public class calculateInterest {
 
         public static void main(String[] args) throws FileNotFoundException {
-            File f1=new File("com/sangamone/Interest.csv");
-            List<String> date1=new ArrayList<>();
-            List<Integer>debit1=new ArrayList<>();
-            List<Integer>credit1=new ArrayList<>();
+            File f1 = new File("C:\\Users\\misba\\intellij\\java-practice\\src\\com\\sangamone\\Interest.csv");
+            List<String> date1 = new ArrayList<>();
+            List<Integer> debit1 = new ArrayList<>();
+            List<Integer> credit1 = new ArrayList<>();
             String[] list1;
+            List<String> date2 = new ArrayList<>();
+            List<Double> interest2=new ArrayList<>();
 
+            DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
+            LocalDate mydate1;
+            LocalDate mydate2;
+            String mydate3;
 
+            for (int i = 0; i < 62; i++) {
 
-            Scanner sc=new Scanner(f1);
-            String info1=sc.nextLine();
-            for(int i=0;i<19;i++) {
-                list1=sc.nextLine().split(",");
+                mydate1 = LocalDate.of(2023, 7, 1);
+                mydate2 = mydate1.plusDays(i);
+                mydate3 = mydate2.format(dtf1);
+                date2.add(mydate3);
+            }
+            System.out.println(date2);
+
+            Scanner sc = new Scanner(f1);
+            String info1 = sc.nextLine();
+            for (int i = 0; i < 19; i++) {
+                list1 = sc.nextLine().split(",");
                 date1.add(list1[0]);
-                if(list1[1]==""){
+                if (list1[1] == "") {
                     credit1.add(0);
-                }
-                else {
+                } else {
                     credit1.add(Integer.parseInt(list1[1]));
                 }
-                if(list1[2]=="") {
+                if (list1[2] == "") {
                     debit1.add(0);
-                }
-                else {
+                } else {
                     debit1.add(Integer.parseInt(list1[2]));
                 }
 
             }
 
-            double intTot=0;
-            String dt1="";
-            int opening1=0;
-            int principal1=0;
-            double time1=1.0/365;
-            double rate1=5.0/100;
-            double interest1=0;
-            int credit=0;
-            int debit=0;
-            int pos=0;
+            double intTot = 0.0;
+            String dt1 = "";
+            int opening1 = 0;
+            int principal1 = 0;
+            double time1 = 1.0 / 365;
+            double rate1 = 5.0 / 100;
+            double interest1 = 0;
+            int credit = 0;
+            int debit = 0;
+            int pos = 0;
 
-            dt1="01-Jul-2023";
-            if(dt1.equals(date1.get(0))) {
-                credit=credit1.get(0);
-                debit=debit1.get(0);
+            for (int i = 0; i < date2.size(); i++) {
+                dt1 = date2.get(i);
+                if (dt1.equals(date1.get(pos))) {
+                    credit = credit1.get(pos);
+                    debit = debit1.get(pos);
+                    pos = pos + 1;
+                } else {
+                    credit = 0;
+                    debit = 0;
+                }
+                opening1 = principal1;
+                principal1 = opening1 + credit - debit;
+                //System.out.println(time1);
+                interest1 = principal1 * time1 * rate1;
+                interest2.add(interest1);
+                intTot+=interest2.get(i);
+                //intTot=intTot+interest2;
             }
-            else {
-                credit=0;
-                debit=0;
-            }
-
-            principal1=opening1+credit-debit;
-            //System.out.println(time1);
-            interest1=principal1*time1*rate1;
-            System.out.println(interest1);
+            System.out.println(intTot);
         }
-
     }
